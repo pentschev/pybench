a24ca5a6fa11cb8f56e715d30a2ae84774b03812
Peter Andreas Entschev <peter@entschev.com>
2019-07-02 04:50:49 -0700

    Add dataframe column filtering utility functions

--- pybench/utils.py
+++ pybench/utils.py
@@ -7,6 +7,15 @@ def benchmark_json_to_pandas(path):
 
     return pd.io.json.json_normalize(data=data['benchmarks'])
 
+
+def filter_by_string_in_column(df, col, val):
+    return df.loc[df[col].str.contains(val)]
+
+
+def filter_by_value_in_column(df, col, val):
+    return df.loc[df[col] == val]
+
+
 def split_params_list(df, params_name, columns=None):
     lst = df[params_name].to_list()
     lst = [[l] if not isinstance(l, list) else l for l in lst]
