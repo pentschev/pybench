75798a29b554a3633a7a837ed9b0ef110a061c80
Peter Andreas Entschev <peter@entschev.com>
2019-07-23 14:47:35 +0200

    Improve compute_speedup and add documentation

--- pybench/utils.py
+++ pybench/utils.py
@@ -1,6 +1,5 @@
 import json
 import pandas as pd
-import warnings
 
 
 def benchmark_json_to_pandas(path):
@@ -9,45 +8,91 @@ def benchmark_json_to_pandas(path):
     return pd.io.json.json_normalize(data=data["benchmarks"])
 
 
-def compute_speedup(slow_df, fast_df, match_list, stats_param, label_list=None):
-    res = []
-    for k, v in slow_df.items():
-        for _, row in v.iterrows():
-            match_dict = {}
-            n = v
-            c = fast_df[k]
-            for m in match_list:
-                n = filter_by_value_in_column(n, m, row[m])
-                c = filter_by_value_in_column(c, m, row[m])
-                match_dict[m] = row[m]
+def compute_speedup(slow_df, fast_df, operation_list, param_list, stats_param):
+    """Computes the speedup of fast operations against slow operations.
 
-            label_dict = {}
-            if label_list is not None:
-                for l in label_list:
-                    label_dict[l] = row[l]
+    Parameters
+    ----------
+    slow_df: pandas.DataFrame
+        The dataframe containing the baseline for slow operations
+    fast_df: pandas.DataFrame
+        The dataframe containing the baseline for fast operations
+    operations_list: list of str
+        A list containing the names of operations for which to compute the
+        speedup. pytest-benchmark populates a field ``name`` with the name of a
+        pytest and the parameters used in that test, which is unique for all
+        parameter combinations of a test. Each pytest function starts with the
+        "test_" prefix and an operation name, such as "Sum", forming a name
+        "test_Sum". This parameter should get a list of operation names, for
+        example:
+            ``["FFT", "Standard_Deviation", "Sum"]``
+    param_list: list of str
+        A list of parameters to identify matching benchmarks on the slow and
+        fast dataframes. If a pytest-benchmark includes a parameter ``shape``
+        which is composed of a list of 2-dimensional tuples, matching rows of
+        the dataframes would have the same value for the two dimensions. The
+        format for each element of that list should be
+        ``"param_name.dimension``. An example for the case above would look
+        like the following:
+            ``["params.shape.0", "params.shape.1"]``
+    stats_param: str
+        The name of the parameter which we would like to use for computing
+        speedups. Since pytest-benchmark computes different metrics, such as
+        min, max, mean, median, etc., the metric desired has to be set.
+        Usually, 'stats.median' is used.
 
-            if c.shape[0] == 0 or n.shape[0] == 0:
-                empty_row = "slow" if n.shape[0] == 0 else "fast"
-                message = "row for operation {0} with {1} not found on {2} dataframe, skipping operation".format(
-                    k, str(match_dict), empty_row
-                )
-                warnings.warn(message, RuntimeWarning)
-            else:
-                n_med = n.iloc[0][stats_param]
-                c_med = c.iloc[0][stats_param]
+    Returns
+    -------
+    pandas.DataFrame
+        A pandas DataFrame containing columns for the name of each operation,
+        the parameters used to match slow and fast operations, and the speedup
+        of the fast operation over the slow one.
 
-                res.append(
-                    pd.DataFrame(
-                        {
-                            "operation": [k],
-                            "speedup": n_med / c_med,
-                            **match_dict,
-                            **label_dict,
-                        }
-                    )
-                )
+    Example
+    -------
+    >>> compute_speedup(numpy_df, cupy_df,
+    >>>     ["FFT", "Standard_Deviation", "Sum"],
+    >>>     ["params.shape.0", "params.shape.1"],
+    >>>     "median")
+    """
+    if not isinstance(operation_list, list):
+        raise TypeError
+    if not isinstance(param_list, list):
+        raise TypeError
+    if not isinstance(stats_param, str):
+        raise TypeError
 
-    return pd.concat(res, ignore_index=True)
+    slow_df = slow_df.loc[
+        slow_df["name"].str.contains("|".join(operation_list))
+    ]
+    fast_df = fast_df.loc[
+        fast_df["name"].str.contains("|".join(operation_list))
+    ]
+
+    slow_df["name"] = slow_df["name"].apply(
+        lambda r: r.split("test_")[1].split("[")[0]
+    )
+    fast_df["name"] = fast_df["name"].apply(
+        lambda r: r.split("test_")[1].split("[")[0]
+    )
+
+    slow_df = slow_df[["name", *param_list, stats_param]]
+    fast_df = fast_df[["name", *param_list, stats_param]]
+
+    speedup_df = slow_df.merge(fast_df, on=["name", *param_list])
+    speedup_df["speedup"] = (
+        speedup_df[stats_param + "_x"] / speedup_df[stats_param + "_y"]
+    )
+    speedup_df["speedup"] = speedup_df["speedup"].apply(
+        lambda r: 1.0 / -r if r < 1 else r
+    )
+    speedup_df["speedup"] = speedup_df["speedup"].apply(
+        significant_round, precision=2
+    )
+
+    speedup_df.drop(columns=[stats_param + "_x", stats_param + "_y"])
+
+    return speedup_df
 
 
 def filter_by_string_in_column(df, col, val):
